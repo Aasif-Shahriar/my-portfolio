16ce9ed407ed3a627da3bc319ffa39ffe022f051
Aasif-Shahriar <asifshahriar247@gmail.com>
2025-08-11 17:12:07 +0600

    Style added: hero/landing section

--- src/pages/home-page/hero-section/Hero.jsx
+++ src/pages/home-page/hero-section/Hero.jsx
@@ -1,39 +1,97 @@
 import React from "react";
+import { FaRegEye } from "react-icons/fa";
+import { GoDotFill } from "react-icons/go";
+import { IoMdArrowForward } from "react-icons/io";
+import { MdOutlineFileDownload } from "react-icons/md";
+import { motion } from "framer-motion";
 
 const Hero = () => {
+  const handleClickScroll = () => {
+    const projectsSection = document.getElementById("projects");
+    if (projectsSection) {
+      projectsSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <section
       id="home"
-      className="relative bg-frame-9 text-frame-1 dark:bg-frame-1 dark:text-frame-9 py-20 overflow-hidden"
+      className="relative bg-frame-9 text-frame-1 dark:bg-frame-1 dark:text-frame-9 overflow-hidden"
     >
       {/* Soft Glow Layer */}
       <div className="absolute inset-0 flex items-center justify-center">
-        <div className="w-[80%] h-[80%] bg-white/10 dark:bg-white/5 blur-3xl rounded-full"></div>
+        <div className="w-[40%] h-[80%] bg-black/10 dark:bg-white/5 blur-3xl rounded-full"></div>
       </div>
 
       {/* Content */}
-      <div className="section-styles text-center relative z-10">
-        <h4 className="text-lg">Hello, I'm</h4>
-        <h1 className="text-5xl font-bold">
-          Asif <span className="text-frame-light-1 dark:text-frame-dark-1">Azad</span>
+      <div className="section-styles flex flex-col justify-center items-center relative z-10 space-y-6">
+        <h4 className="text-lg text-frame-3 dark:text-frame-6 font-semibold">
+          Hello, I'm
+        </h4>
+        <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-frame-2 dark:text-frame-8">
+          Asif{" "}
+          <span className="text-frame-light-1 dark:text-frame-dark-1">
+            Azad
+          </span>
         </h1>
-        <h3 className="text-2xl mt-2">Full Stack Developer</h3>
-        <p className="max-w-xl mx-auto mt-4 text-frame-2">
+        <h3 className="text-2xl md:text-4xl font-light mt-2">
+          MERN Stack Developer
+        </h3>
+        {/* slogan */}
+        <p className="text-center text-frame-2 dark:text-frame-8 md:text-xl">
           Crafting Interactive Web Experiences with the MERN Stack
         </p>
-        <div className="flex gap-4 justify-center mt-6">
-          <button className="px-6 py-3 bg-frame-light-1 dark:bg-frame-dark-1 text-white rounded-md">
+        {/* view & resume btn */}
+        <div className="flex flex-col md:flex-row gap-4 justify-center mt-6">
+          <button onClick={handleClickScroll} className="btn-primary px-6 py-3">
+            <FaRegEye size={20} />
             View My Work
+            <IoMdArrowForward size={20} />
           </button>
-          <button className="px-6 py-3 border border-frame-light-1 dark:border-frame-dark-1 rounded-md">
+          <a
+            href="/Asif_Azad_Resume.pdf"
+            download="Asif_Azad_Resume.pdf"
+            className="btn-outline px-6 py-3"
+          >
+            <MdOutlineFileDownload size={20} />
             Download Resume
-          </button>
+          </a>
         </div>
-        <div className="mt-6 text-sm opacity-80">
-          <p>Available for freelance work</p>
+        {/* message & location */}
+        <div className="flex flex-col md:flex-row items-center gap-6 mt-6 text-frame-3 dark:text-frame-5">
+          <p className="flex items-center gap-2">
+            <motion.span
+              animate={{ color: ["#0a7e5f", "#22c55e", "#0a7e5f"] }}
+              transition={{
+                duration: 2,
+                repeat: Infinity,
+                repeatType: "loop",
+                ease: "easeInOut",
+              }}
+              className="text-green-500"
+            >
+              <GoDotFill />
+            </motion.span>
+            Available for freelance work
+          </p>
+          <div className="hidden md:block h-5 border-r border-frame-2 dark:border-frame-8"></div>
           <p>Based in Dhaka, Bangladesh</p>
         </div>
-        <div className="mt-8">Scroll</div>
+        {/* animate up and down */}
+        <motion.div
+          className="flex flex-col items-center"
+          animate={{ y: [0, -10, 0] }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+            repeatType: "loop",
+            ease: "easeInOut",
+          }}
+        >
+          <p className="uppercase text-frame-2 dark:text-frame-7 text-xs">
+            Scroll
+          </p>
+          <div className="h-12 w-[1px] bg-gradient-to-b from-frame-1 to-frame-9 dark:from-frame-9 dark:to-frame-1"></div>
+        </motion.div>
       </div>
     </section>
   );
